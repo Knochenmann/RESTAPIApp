0b87b3c3a49bf1b4696481472976310244b16dca
Egor Kostyukhin <knochenmann.dev@gmail.com>
2022-01-09 14:16:48 +0300

    Initial Commit

---
+++ RESTAPIApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  RESTAPIApp
+//
+//  Created by Егор Костюхин on 09.01.2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
